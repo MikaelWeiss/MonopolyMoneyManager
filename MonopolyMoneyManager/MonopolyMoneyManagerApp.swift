9190dfaa1afd68828fe996565802d0bbc7029475
Mikael Weiss <campingmikael@icloud.com>
2020-12-23 10:11:29 -0700

    Initial Commit

---
+++ MonopolyMoneyManager/MonopolyMoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyMoneyManagerApp.swift
+//  MonopolyMoneyManager
+//
+//  Created by Mikael Weiss on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyMoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
